2b579d00ae8bf72ca2a5fe346f162b418cecc453
Fatih Emre <112892044+blaineconnor@users.noreply.github.com>
2025-01-13 17:05:44 +0300

    Initial Commit

---
+++ Grocery List/Grocery_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_ListApp.swift
+//  Grocery List
+//
+//  Created by Fatih Emre on 13.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
