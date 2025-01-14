2999a6b104e9bb7f0da6de06765ae5bd4ba53552
Fatih Emre <112892044+blaineconnor@users.noreply.github.com>
2025-01-14 11:39:45 +0300

    feat: popover hints implemented to code and delete all button has added.

--- Grocery List/ContentView.swift
+++ Grocery List/ContentView.swift
@@ -7,10 +7,33 @@
 
 import SwiftUI
 import SwiftData
+import TipKit
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
+    
+    @State private var item: String = ""
+    
+    @FocusState private var isFocused: Bool
+    
+    let buttonTip = ButtonTip()
+    
+    func setupTips(){
+        do {
+            try Tips.resetDatastore()
+            Tips.showAllTipsForTesting()
+            try Tips.configure([
+                .displayFrequency(.immediate)
+            ])
+        } catch {
+            print ("Error initializing TipKit \(error.localizedDescription)")
+        }
+    }
+    
+    init() {
+        setupTips()
+    }
     
     func addEssentialFoods() {
         modelContext.insert( Item(title: "Fasulye", isCompleted: false))
@@ -19,6 +42,14 @@ struct ContentView: View {
         modelContext.insert(Item(title: "Süt", isCompleted: .random()))
         modelContext.insert(Item(title: "Peynir ve Yumurta", isCompleted: .random()))
         modelContext.insert(Item(title: "Kuruyemiş", isCompleted: .random()))
+    }
+    
+    func deleteAllItems() {
+        withAnimation {
+            for item in items {
+                modelContext.delete(item)
+            }
+        }
     }
     
     var body: some View {
@@ -56,10 +87,49 @@ struct ContentView: View {
                         Button {
                             addEssentialFoods()
                         } label: {
-                            Label("Essentials", systemImage: "carrot")
+                            Image(systemName: "carrot")
+                        }
+                        .popoverTip(buttonTip)
+                    }
+                } else {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button(role: .destructive) {
+                            deleteAllItems()
+                        } label: {
+                            Label("Delete All", systemImage: "trash")
                         }
                     }
                 }
+            }
+            .safeAreaInset(edge: .bottom) {
+                VStack(spacing: 12) {
+                    TextField("", text: $item)
+                        .textFieldStyle(.plain)
+                        .padding(12)
+                        .background(.tertiary)
+                        .cornerRadius(12)
+                        .font(.title.weight(.light))
+                        .focused($isFocused)
+                    Button {
+                        guard !item.isEmpty else {
+                            return
+                        }
+                        
+                        let newItem = Item(title: item, isCompleted: false)
+                        modelContext.insert(newItem)
+                        item = ""
+                        isFocused = false
+                    } label: {
+                        Text("Save")
+                            .font(.title2.weight(.medium))
+                            .frame(maxWidth: .infinity)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .buttonBorderShape(.roundedRectangle)
+                    .controlSize(.extraLarge)
+                }
+                .padding()
+                .background(.bar)
             }
             .overlay {
                 if items.isEmpty {
