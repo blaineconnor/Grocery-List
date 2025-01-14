928e439c70ae13caaf59eb686e7efacafb848664
Fatih Emre <112892044+blaineconnor@users.noreply.github.com>
2025-01-14 11:49:08 +0300

    Fix: Text edited.

--- Grocery List/Tip.swift
+++ Grocery List/Tip.swift
@@ -9,6 +9,6 @@ import TipKit
 
 struct ButtonTip: Tip {
     var title: Text = Text("Essential Foods")
-    var message: Text? = Text("add some everyday items to the shopping list.")
+    var message: Text? = Text("Add some everyday items to the shopping list.")
     var image: Image? = Image(systemName: "info.circle")
 }
